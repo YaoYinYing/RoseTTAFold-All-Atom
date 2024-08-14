666d233000a681843976911e8b34cd87f9da86a2
YaoYinYing <33014714+YaoYinYing@users.noreply.github.com>
2024-08-14 10:10:23 +0800

    chore: msa to Python wrappers

--- rf2aa/data/preprocessing.py
+++ rf2aa/data/preprocessing.py
@@ -1,43 +1,57 @@
 import os
-from hydra import initialize, compose
-from pathlib import Path
-import subprocess
+import shutil
 
-#from rf2aa.run_inference import ModelRunner
-script_dir=os.path.dirname(os.path.abspath(__file__)) #RoseTTAFold-All-Atom/rf2aa/data
-msa_script_dir=os.path.abspath(os.path.join(script_dir, '..','input_prep'))   #RoseTTAFold-All-Atom/rf2aa/input_prep
+from omegaconf import DictConfig
+
+from rf2aa.data.msa.pipeline import Pipeline
 
 
-def make_msa(
-    fasta_file,
-    chain,
-    model_runner
-): 
-    out_dir_base = Path(model_runner.config.output_path)
-    hash = model_runner.config.job_name
-    out_dir = out_dir_base / hash / chain
-    out_dir.mkdir(parents=True, exist_ok=True)
+def make_msa(fasta_file: str, chain: str, model_runner: DictConfig):
 
-    command = model_runner.config.database_params.command
+    CONDA_PREFIX = os.environ.get("CONDA_PREFIX", None)
+    if CONDA_PREFIX is None:
+        raise NotImplementedError("Make sure conda is activated")
 
-# sequence databases
-    DB_UR30=model_runner.config.database_params.DB_UR30
-    DB_BFD=model_runner.config.database_params.DB_BFD
+    out_dir_base = os.path.abspath(model_runner.config.output_path)
+    hash: str = model_runner.config.job_name
+    out_dir = os.path.join(out_dir_base, hash, chain)
+    os.makedirs(out_dir, exist_ok=True)
+
+    # sequence databases
+    DB_UR30 = model_runner.config.database_params.DB_UR30
+    DB_BFD = model_runner.config.database_params.DB_BFD
     num_cpus = model_runner.config.database_params.num_cpus
     ram_gb = model_runner.config.database_params.mem
     template_database = model_runner.config.database_params.DB_PDB100
 
-    out_a3m = out_dir / "t000_.msa0.a3m"
-    out_atab = out_dir / "t000_.atab"
-    out_hhr = out_dir / "t000_.hhr"
-    if out_a3m.exists() and out_atab.exists() and out_hhr.exists():
-        return out_a3m, out_hhr, out_atab
+    msa_pipeline = Pipeline(
+        hhblits_binary=shutil.which("hhblits"),
+        hhfilter_binary=shutil.which("hhfilter"),
+        hhsearch_binary=shutil.which("hhsearch"),
+        signalp_binary=shutil.which("signalp"),
+        makemat_binary=shutil.which("makemat"),
+        blast_path=os.path.join(CONDA_PREFIX, "share/blast-2.2.26/blast-2.2.26"),
+        psipred_binary=shutil.which("psipred"),
+        psipred_data_dir=os.path.join(CONDA_PREFIX, "share", "psipred_4.01/data"),
+        psipass2_binary=shutil.which("psipass2"),
+        csblast_dir=os.path.join(CONDA_PREFIX, "share/csblast-2.2.3"),
+        uniref30_database=DB_UR30,
+        bfd_databse=DB_BFD,
+        template_database=template_database,
+        ncpu=num_cpus,
+        max_mem=ram_gb,
+        out_prefix="rf2aa_",
+        save_dir=out_dir,
+    )
 
-    search_command = f"{msa_script_dir}/{command} {os.path.abspath(fasta_file)} {os.path.abspath(out_dir)} {num_cpus} {ram_gb} {DB_UR30} {DB_BFD} {template_database}"
-    print(search_command)
-    _ = subprocess.run(search_command, shell=True)
+    trim_fasta = msa_pipeline.run_signalp(fasta_path=os.path.abspath(fasta_file))
 
-    if _.returncode != 0:
-        raise RuntimeError(f"Failed to execute command {search_command}")
+    out_a3m = msa_pipeline.run_msa_search(fasta_path=trim_fasta)
+    ss2_file = msa_pipeline.run_psipred(msa_path=out_a3m)
+    out_hhr, out_atab = msa_pipeline.run_hhsearch(msa_path=out_a3m, ss2_path=ss2_file)
+
+    for f in [out_a3m, out_hhr, out_atab]:
+        if not os.path.isfile(f):
+            raise FileNotFoundError(f"{f} not found")
+
     return out_a3m, out_hhr, out_atab
-
